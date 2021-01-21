683406372ca9a90f22186889a1d76dfdea546e5f
mobarakol <mobarakol@u.nus.edu>
2021-01-21 00:32:39 +0000

    new

--- unet_model/__init__.py
+++ unet_model/__init__.py
@@ -1,3 +1,3 @@
-from unet_model.augmentation_blocks import DataAugmenter
-from unet_model.layers import get_norm_layer
-from unet_model.unet import Unet,  EquiUnet, Att_EquiUnet
+from UNetLike.unet_model.augmentation_blocks import DataAugmenter
+from UNetLike.unet_model.layers import get_norm_layer
+from UNetLike.unet_model.unet import Unet,  EquiUnet, Att_EquiUnet
