669ce48c055f6d985451d9d681ef255a5995ed48
Mobarakol Islam <mobarakol@u.nus.edu>
2021-02-20 22:01:09 +0000

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -176,7 +176,70 @@ class MNMS(Dataset):
     def __len__(self):
         return len(self.datas)
     
+class Brats(Dataset):
+    def __init__(self, patients_dir, benchmarking=False, training=True, data_aug=False,
+                 no_seg=False, normalisation="minmax"):
+        super(Brats, self).__init__()
+        self.benchmarking = benchmarking
+        self.normalisation = normalisation
+        self.data_aug = data_aug
+        self.training = training
+        self.datas = []
+        self.validation = no_seg
+        self.patterns = ["_t1", "_t1ce", "_t2", "_flair"]
+        if not no_seg:
+            self.patterns += ["_seg"]
+        for patient_dir in patients_dir:
+            patient_id = patient_dir.name
+            paths = [patient_dir / f"{patient_id}{value}.nii.gz" for value in self.patterns]
+            patient = dict(
+                id=patient_id, t1=paths[0], t1ce=paths[1],
+                t2=paths[2], flair=paths[3], seg=paths[4] if not no_seg else None
+            )
+            self.datas.append(patient)
 
+    def __getitem__(self, idx):
+        _patient = self.datas[idx]
+        patient_image = {key: self.load_nii(_patient[key]) for key in _patient if key not in ["id", "seg"]}
+        if _patient["seg"] is not None:
+            patient_label = self.load_nii(_patient["seg"])
+        if self.normalisation == "minmax":
+            patient_image = {key: irm_min_max_preprocess(patient_image[key]) for key in patient_image}
+        elif self.normalisation == "zscore":
+            patient_image = {key: zscore_normalise(patient_image[key]) for key in patient_image}
+        patient_image = np.stack([patient_image[key] for key in patient_image])
+        patient_label[patient_label==4] = 3
+        patient_label = patient_label[None,:,:,:]
+        if self.training:
+            # Remove maximum extent of the zero-background to make future crop more useful
+            z_indexes, y_indexes, x_indexes = np.nonzero(np.sum(patient_image, axis=0) != 0)
+            # Add 1 pixel in each side
+            zmin, ymin, xmin = [max(0, int(np.min(arr) - 1)) for arr in (z_indexes, y_indexes, x_indexes)]
+            zmax, ymax, xmax = [int(np.max(arr) + 1) for arr in (z_indexes, y_indexes, x_indexes)]
+            patient_image = patient_image[:, zmin:zmax, ymin:ymax, xmin:xmax]
+            patient_label = patient_label[:, zmin:zmax, ymin:ymax, xmin:xmax]
+            # default to 128, 128, 128
+            patient_image, patient_label = pad_or_crop_image(patient_image, patient_label, target_size=(128, 128, 128))
+        else:
+            z_indexes, y_indexes, x_indexes = np.nonzero(np.sum(patient_image, axis=0) != 0)
+            # Add 1 pixel in each side
+            zmin, ymin, xmin = [max(0, int(np.min(arr) - 1)) for arr in (z_indexes, y_indexes, x_indexes)]
+            zmax, ymax, xmax = [int(np.max(arr) + 1) for arr in (z_indexes, y_indexes, x_indexes)]
+            patient_image = patient_image[:, zmin:zmax, ymin:ymax, xmin:xmax]
+            patient_label = patient_label[:, zmin:zmax, ymin:ymax, xmin:xmax]
+        
+        patient_image, patient_label = patient_image.astype("float16"), patient_label.astype("long")
+        patient_image, patient_label = [torch.from_numpy(x) for x in [patient_image, patient_label]]
+        # onehot
+        #patient_label_oh = (patient_label.squeeze()[...,None] == torch.arange(4).reshape(1, 4))
+        #patient_label_oh = patient_label_oh.permute(3,0,1,2).float()
+        return dict(patient_id=_patient["id"],
+                    image=patient_image, label=patient_label,
+                    seg_path=str(_patient["seg"]), #if not self.validation else str(_patient["t1"]),
+                    crop_indexes=((zmin, zmax), (ymin, ymax), (xmin, xmax)),
+                    #et_present=0,
+                    #supervised=True,
+                    )
 
     
 def get_datasets19(seed, data_root='/media/mobarak/data/Datasets/MNMS', no_seg=False, on="train", full=False,
@@ -196,6 +259,14 @@ def get_datasets19(seed, data_root='/media/mobarak/data/Datasets/MNMS', no_seg=F
     #bench_dataset = MNMS(patients_dir_valid, training=False, normalisation=normalisation, ids_vendors=patient_ids_vendors)
     return  val_dataset#, bench_dataset
 
+def get_datasets_BraTS19(seed, data_root=None, no_seg=False, on="train", full=False,
+                 fold_number=0, normalisation="minmax"):
+
+    data_root = pathlib.Path(data_root)
+    base_folder_valid = pathlib.Path('cv_valid/').resolve()
+    patients_dir_valid = sorted([data_root/x.name for x in base_folder_valid.iterdir() if (data_root/x.name).is_dir()])
+    val_dataset = Brats(patients_dir_valid, training=False, data_aug=False, normalisation=normalisation)
+    return val_dataset
 
 
 
